064ff9259dc96ed6b762083f3804b4fd0fc7658c
olhkyle <kylekwon96@gmail.com>
2023-11-09 21:06:50 +0900

    [FEAT] implement user type definition

--- src/recoil/atoms/userState.ts
+++ src/recoil/atoms/userState.ts
@@ -1,17 +1,38 @@
-import { atom } from 'recoil';
+import avatars from 'constant/avatars';
+import { AtomEffect, atom } from 'recoil';
+
+interface User {
+	email: string;
+	nickName: string;
+	firstName: string;
+	lastName: string;
+	level: number;
+	point: number;
+	avatarId: keyof typeof avatars;
+}
 
 const KEY = 'user';
 
-const localStorageEffect = ({ onSet }) => {
-  onSet(newState => {
-    localStorage.setItem(KEY, JSON.stringify(newState));
-  });
-};
+const localStorageEffect: <T>(key: string) => AtomEffect<T | null> =
+	(key: string) =>
+	({ setSelf, onSet }) => {
+		const user = localStorage.getItem(key);
+
+		if (user !== null) {
+			// initialize atom value
+			setSelf(JSON.parse(user));
+		}
+
+		// reset atom value
+		onSet((newState, _, isReset) => {
+			isReset ? localStorage.removeItem(key) : localStorage.setItem(key, JSON.stringify(newState));
+		});
+	};
 
 const userState = atom({
-  key: 'userState',
-  default: JSON.parse(localStorage.getItem(KEY)),
-  effects: [localStorageEffect],
+	key: 'userState',
+	default: null,
+	effects: [localStorageEffect<User>(KEY)],
 });
 
 export default userState;
